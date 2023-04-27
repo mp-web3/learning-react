bcd7b47793317d365f74a33f429f9c6b70699c6b
mp-web3 <mp.web3@gmail.com>
2023-04-27 14:55:57 +0200

    set up all of the files

--- CodeyForum/src/App.js
+++ CodeyForum/src/App.js
@@ -1,23 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {comments} from './commentData';
+import Body from './Body'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Change <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Body comment={comments[0].comment}/>
     </div>
   );
 }
