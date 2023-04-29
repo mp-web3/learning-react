f769ade2aa6fd8ca0538058ccca5aef7bf01ec07
mp-web3 <mp.web3@gmail.com>
2023-04-29 14:18:21 +0200

    Jamming project environment set up

---
+++ Jamming/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+
+	return (
+      <div>
+      </div>
+    );
+};
+
+export default App;
---
+++ Jamming/src/Playlist.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Playlist() {
+    return (
+      <div>
+      </div>
+    );
+};
+export default Playlist;
