863c6aee4567c37ea8270aa017256f83c0f42097
mp-web3 <mp.web3@gmail.com>
2023-05-04 15:47:58 +0200

    Created Playlist component

--- Jamming/src/components/Playlist/Playlist.js
+++ Jamming/src/components/Playlist/Playlist.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import TrackList from "../SearchResults/TrackList";
 
-function Playlist() {
-    return (
-      <div>
-      </div>
-    );
-};
+class Playlist extends React.Component {
+
+    render() {
+
+        const { playlistTracks, onRemove, onSave } = this.props
+
+        return (
+            <div className='Playlist'>
+                <input defaultValue={'New Playlist'} />
+
+                <TrackList 
+                tracks={playlistTracks}
+                onRemove={onRemove}
+                isRemoval={true} />
+
+                <button className="Playlist-save" onCLick={onSave}>Save Playlist</button>
+
+            </div>
+
+        );
+    }
+}
+
 export default Playlist;
--- Jamming/src/components/Playlist/Tracklist.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Tracklist() {
-    return (
-      <div>
-      </div>
-    );
-};
-export default Tracklist;
