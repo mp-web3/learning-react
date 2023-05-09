a09fec50c93dc9f719929c18c922ffc61b9ef4e7
mp-web3 <mp.web3@gmail.com>
2023-05-09 11:15:44 +0200

    completing Spotify object to search for track

--- Jamming/src/util/Spotify.js
+++ Jamming/src/util/Spotify.js
@@ -1,4 +1,27 @@
-const accessToken='';
-const Spotify = {};
+const accessToken = '';
+const clientID = '1a4ea45a5e734a3f9753c2ea1c4c4ffc';
+const redirectURI = 'http://localhost:3000/';
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+        const urlAccessToken = window.location.href.match(/access_token=([^&]*)/);
+        const urlExpiresIn = window.location.href.match(/expires_in=([^&]*)/);
+        if (urlAccessToken && urlExpiresIn) {
+            accessToken = urlAccessToken[1];
+            const expiresIn = Number(urlExpiresIn[1]);
+            window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+        } else {
+            const redirect ='https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}';
+            window.location = redirect;
+        }
+    },
+
+    search(term) {
+        return fetch('https://api.spotify.com/v1/search?type=track&q=${term}')
+    }
+};
 
 export { Spotify };
---
+++ Jamming/src/util/Test.js
@@ -0,0 +1,3 @@
+const exampleURL = 'https://example.com/callback#access_token=NwAExz...BV302Tk&token_type=Bearer&expires_in=3600&state=123';
+const match = exampleURL.match(/access_token=([^&]*)/);
+console.log(match[1]);
