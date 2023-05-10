a70532d28f67dce2d955e1e0aa602d095eaf2a11
mp-web3 <mp.web3@gmail.com>
2023-05-10 18:01:11 +0200

    trying to fix invalid token

--- Jamming/src/util/Spotify.js
+++ Jamming/src/util/Spotify.js
@@ -1,9 +1,12 @@
 let accessToken = null;
+let tokenExpirationTime = null;
+
 const clientID = '1a4ea45a5e734a3f9753c2ea1c4c4ffc';
 const redirectURI = 'http://localhost:3000/';
+
 const Spotify = {
     getAccessToken() {
-        if (accessToken) {
+        if (accessToken && !this.isTokenExpired()) {
             return accessToken;
         }
         const urlAccessToken = window.location.href.match(/access_token=([^&]*)/);
@@ -11,12 +14,21 @@ const Spotify = {
         if (urlAccessToken && urlExpiresIn) {
             accessToken = urlAccessToken[1];
             const expiresIn = Number(urlExpiresIn[1]);
+            tokenExpirationTime = new Date().getTime() + expiresIn * 1000;
             window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
         } else {
             const redirect =`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
             window.location = redirect;
         }
+    },
+
+    isTokenExpired() {
+        if (!tokenExpirationTime) {
+            return true;
+        }
+        const currentTime = new Date().getTime();
+        return currentTime >= tokenExpirationTime;
     },
 
     search(term) {
